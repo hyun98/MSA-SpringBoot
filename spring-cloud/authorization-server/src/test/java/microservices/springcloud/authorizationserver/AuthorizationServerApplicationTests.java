b0fb45daa81c0574835a66f3bd0225c0c45f0c01
hyun0404woo <hyun0404woo@gmail.com>
2022-06-09 00:30:40 +0900

    Feat: Spring Security Init

---
+++ spring-cloud/authorization-server/src/test/java/microservices/springcloud/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.springcloud.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
