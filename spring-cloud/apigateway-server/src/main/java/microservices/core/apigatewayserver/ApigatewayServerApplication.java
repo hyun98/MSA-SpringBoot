f183fd834c7f212055af9e64e3dab357c590978e
hyun0404woo <hyun0404woo@gmail.com>
2022-06-15 18:41:47 +0900

    Feat: Init Spring Cloud Gateway
    - For APIGateway

---
+++ spring-cloud/apigateway-server/src/main/java/microservices/core/apigatewayserver/ApigatewayServerApplication.java
@@ -0,0 +1,13 @@
+package microservices.core.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayServerApplication.class, args);
+	}
+
+}
