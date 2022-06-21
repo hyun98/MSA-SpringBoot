4d5acba5f22ef66a98813df879b9551bcc828a6f
hyun0404woo <hyun0404woo@gmail.com>
2022-06-21 16:43:14 +0900

    Feat: Use @ActiveProfile in TestCode
    - Integrate application.yml

--- microservices/product-composite-service/src/test/java/microservices/core/productcomposite/ProductCompositeServiceApplicationTests.java
+++ microservices/product-composite-service/src/test/java/microservices/core/productcomposite/ProductCompositeServiceApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.reactive.server.WebTestClient.BodyContentSpec;
 import reactor.core.publisher.Flux;
@@ -31,7 +32,7 @@ import static reactor.core.publisher.Mono.just;
 
 @SpringBootTest(
 	webEnvironment = RANDOM_PORT)
-@Import(TestSecurityConfig.class)
+@ActiveProfiles("test")
 class ProductCompositeServiceApplicationTests {
 	private static final int PRODUCT_ID_OK = 1;
 	private static final int PRODUCT_ID_NOT_FOUND = 2;
