950306a8efcb22ea211472c0e0296e9ad57a4aa6
hyun0404woo <hyun0404woo@gmail.com>
2022-06-21 16:45:18 +0900

    Feat: Use Tomcat instead of Netty; while Testing
    - NonBlocking service evoke with Tomcat

--- microservices/recommendation-service/src/main/generated/microservices/core/recommendation/services/RecommendationMapperImpl.java
+++ microservices/recommendation-service/src/main/generated/microservices/core/recommendation/services/RecommendationMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-25T14:16:27+0900",
+    date = "2022-06-21T15:57:42+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
--- microservices/review-service/src/main/generated/microservices/core/review/services/ReviewMapperImpl.java
+++ microservices/review-service/src/main/generated/microservices/core/review/services/ReviewMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-25T13:56:52+0900",
+    date = "2022-06-21T15:57:36+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (AdoptOpenJDK)"
 )
 @Component
